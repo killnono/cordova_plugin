0f177624e7a2ff44b0e68bfc4e86e807cd805c33
killnono <ckcanfly@gmail.com>
2015-10-21 12:56:43 +0800

    first

---
+++ www/YCProblemSetPlugin.js
@@ -0,0 +1,20 @@
+function YCProblemSetPlugin() {
+}
+
+YCProblemSetPlugin.prototype.loadProblem = function (url, options) {
+	options = options || {};
+	cordova.exec(options.successCallback || null, options.errorCallback || null, "YCProblemSetPlugin", "loadProblem", [url, options]);
+};
+YCProblemSetPlugin.prototype.loadImage = function (url, options) {
+	options = options || {};
+	cordova.exec(options.successCallback || null, options.errorCallback || null, "YCProblemSetPlugin", "loadImage", [url, options]);
+};
+
+
+YCProblemSetPlugin.install = function () {
+	if (!window.plugins) {
+		window.plugins = {};
+	}
+	window.plugins.YCProblemSetPlugin = new YCProblemSetPlugin();
+	return window.plugins.YCProblemSetPlugin;
+};
